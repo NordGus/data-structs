46b521492301821bb1fcb2fd4e04aa10b488af19
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-06-10 15:32:49 +0200

    solving QueueReverser exercise

--- src/04_queues/QueueReverser.test.ts
+++ src/04_queues/QueueReverser.test.ts
@@ -2,21 +2,68 @@ import Queue from "./Queue";
 import QueueReverser from "./QueueReverser";
 
 describe("QueueReverser", () => {
-  let queue: Queue;
+  let queue: Queue<number>;
   const size = 5;
   const values = [1, 2, 3, 4, 5];
-  const reversed = [5, 4, 3, 2, 1];
 
   beforeEach(() => (queue = new Queue(size)));
   beforeEach(() => values.forEach((i: number) => queue.add(i)));
 
-  it("recieves a Queue and reverse it", () => {
-    let rev = new QueueReverser();
+  describe("no K value given", () => {
+    it("reverses the given Queue", () => {
+      let rev = new QueueReverser<number>(queue);
 
-    expect(queue.toArray()).toEqual(values);
+      expect(queue.toArray()).toEqual(values);
 
-    rev.reverse(queue);
+      rev.reverse();
 
-    expect(queue.toArray()).toEqual(reversed);
+      expect(queue.toArray()).toEqual([5, 4, 3, 2, 1]);
+    });
+  });
+
+  describe("K value given", () => {
+    it("reverses the first K elements in the given Queue", () => {
+      let rev = new QueueReverser<number>(queue, 3);
+
+      expect(queue.toArray()).toEqual(values);
+
+      rev.reverse();
+
+      expect(queue.toArray()).toEqual([3, 2, 1, 4, 5]);
+    });
+
+    it("doens't reverse the given Queue if K is 0", () => {
+      let rev = new QueueReverser<number>(queue, 0);
+
+      expect(queue.toArray()).toEqual(values);
+
+      rev.reverse();
+
+      expect(queue.toArray()).toEqual(values);
+    });
+
+    it("throws an illegal argument error if K < 0", () => {
+      let error = "";
+
+      try {
+        const _ = new QueueReverser<number>(queue, -3);
+      } catch (err) {
+        error = err.message;
+      }
+
+      expect(error).toBe("illegal argument");
+    });
+
+    it("throws an illegal argument error if K > Queue.capacity", () => {
+      let error = "";
+
+      try {
+        const _ = new QueueReverser<number>(queue, 100);
+      } catch (err) {
+        error = err.message;
+      }
+
+      expect(error).toBe("illegal argument");
+    });
   });
 });
--- src/04_queues/QueueReverser.ts
+++ src/04_queues/QueueReverser.ts
@@ -1,16 +1,44 @@
-import Stack from "../03_stacks/Stack";
+import Stack from "../shared/SimpleStack";
 import Queue from "./Queue";
 
-class QueueReverser {
-  private stack: Stack;
+class QueueReverser<T> {
+  private stack: Stack<T>;
+  private queue: Queue<T>;
+  private k: number | null;
 
-  constructor() {
-    this.stack = new Stack();
+  constructor(queue: Queue<T>, k?: number) {
+    if (k && k < 0) throw new Error("illegal argument");
+    if (k && k > queue.capacity) throw new Error("illegal argument");
+
+    this.stack = new Stack<T>();
+    this.queue = queue;
+    this.k = k;
   }
 
-  reverse(queue: Queue): void {
-    for (; !queue.isEmpty(); ) this.stack.push(queue.remove());
-    for (; !this.stack.empty(); ) queue.add(this.stack.pop());
+  reverse(): void {
+    if (this.k >= 0) this.kReverse();
+    else this.nonKReverse();
+  }
+
+  private kReverse(): void {
+    if (this.k === 0) return;
+
+    let tmp = new Queue<T>(this.queue.capacity - this.k);
+
+    for (let i = 1; !this.queue.isEmpty(); i++) {
+      if (i <= this.k) this.stack.push(this.queue.remove());
+      else tmp.add(this.queue.remove());
+    }
+
+    for (let i = 0; i < this.queue.capacity; i++) {
+      if (!this.stack.isEmpty()) this.queue.add(this.stack.pop());
+      else this.queue.add(tmp.remove());
+    }
+  }
+
+  private nonKReverse(): void {
+    for (; !this.queue.isEmpty(); ) this.stack.push(this.queue.remove());
+    for (; !this.stack.isEmpty(); ) this.queue.add(this.stack.pop());
   }
 }
 
