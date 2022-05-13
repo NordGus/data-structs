8e551cb1ff02d2a959c1936c6c4a700da6553cd6
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2022-05-13 20:39:38 +0000

    cleaning code

--- src/11_sorting_algorithms/InsertionSort.ts
+++ src/11_sorting_algorithms/InsertionSort.ts
@@ -16,22 +16,6 @@ class InsertionSort {
 
         return output;
     }
-
-    private swap(array: number[], from: number, to: number): void {
-        const temp = array[from];
-        array[from] = array[to];
-        array[to] = temp;
-    }
-
-    private findMinIndex(array: number[], from: number): number {
-        let minIndex = from;
-
-        for (let i = from; i < array.length; i++)
-            if (array[i] < array[minIndex])
-                minIndex = i;
-
-        return minIndex
-    }
 }
 
 export default InsertionSort;
