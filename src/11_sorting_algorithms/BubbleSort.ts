4f8074731918822ef381109db712bf03ccd2d135
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2022-05-13 09:54:14 +0000

    optimizing bubble sort

--- src/11_sorting_algorithms/BubbleSort.ts
+++ src/11_sorting_algorithms/BubbleSort.ts
@@ -1,15 +1,21 @@
 class BubbleSort {
     public sort(input: number[]): number[] {
         const output = [...input];
+        let sorted = true;
 
         for (let i = 0; i < output.length; i++) {
+            sorted = true
+
             for (let j = 1; j < output.length; j++) {
                 if (output[j] > output[j - 1]) continue;
                 
                 const temp = output[j];
                 output[j] = output[j - 1];
                 output[j - 1] = temp;
+                sorted = false
             }
+
+            if (sorted) break;
         }
 
         return output;
